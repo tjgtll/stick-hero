af688122f2a3b377d01daca14199c2385f9e6619
Nicholas <urkona@list.ru>
2023-10-02 01:18:26 +0300

    feat: update v002

--- assets/scripts/gameManager.ts
+++ assets/scripts/gameManager.ts
@@ -1,10 +1,25 @@
-import { _decorator,director,find,randomRange, view, Component, tween, Node, Label, UITransform, Quat, input, Input,EventTouch, KeyCode, EventKeyboard, macro, Vec3, Vec2, RigidBody2D, BoxCollider2D, Contact2DType, IPhysics2DContact } from 'cc';
+import { 
+    _decorator,
+    director,
+    find,
+    randomRange, 
+    view, 
+    Component, 
+    tween, 
+    Node, 
+    Label, 
+    UITransform, 
+    Quat, 
+    input, 
+    Input,
+    EventTouch, 
+    KeyCode, 
+    EventKeyboard,  
+    Vec3, } from 'cc';
 
 const { ccclass, property } = _decorator;
 
 @ccclass('gameManager')
-
-
 
 export class gameManager extends Component {
     score:                      number = -1;
@@ -34,10 +49,10 @@ export class gameManager extends Component {
     platformEnd:                Node = null;
     @property(Node)
     background:                 Node = null;
+    @property(Node)
+    perfect:                    Node = null;
     @property(Label)
     scoreLabel:                 Label = null;
-
-    
 
     onLoad() {
         input.on(Input.EventType.KEY_DOWN,      this.onKeyDown,     this);
@@ -52,6 +67,8 @@ export class gameManager extends Component {
         this.stickStartTransform         = this.stickStart.getComponent(UITransform);
         this.stickEndTransform           = this.stickEnd.getComponent(UITransform);
         this.playerTransform             = this.player.getComponent(UITransform);
+
+        this.movePlatformEnd();
     }
 
     onDestroy() {
@@ -63,7 +80,7 @@ export class gameManager extends Component {
     onTouchStart(event: EventTouch) {
         if (!this.isAnimating) {
             this.isResizing = true;
-            this.schedule(this.resize, 0);
+            this.schedule(this.resizeStick, 0);
             this.isKeyDown = true;
         }
     }
@@ -72,7 +89,7 @@ export class gameManager extends Component {
         if (!this.isAnimating && this.isKeyDown) {
             this.isAnimating = true;
             this.isResizing = false;
-            this.unschedule(this.resize);
+            this.unschedule(this.resizeStick);
             this.handleGameResult();    
             this.isKeyDown = false;
         }
@@ -85,7 +102,7 @@ export class gameManager extends Component {
                 if (!this.isAnimating) {
                     this.isResizing = true;
                     this.isKeyDown = true;
-                    this.schedule(this.resize, 0);
+                    this.schedule(this.resizeStick, 0);
                 }
                 break;
             }
@@ -104,7 +121,7 @@ export class gameManager extends Component {
                 if (!this.isAnimating && this.isKeyDown) {
                     this.isAnimating = true;
                     this.isResizing = false;
-                    this.unschedule(this.resize);
+                    this.unschedule(this.resizeStick);
                     this.handleGameResult();    
                     this.isKeyDown = false;
                 }
@@ -122,6 +139,13 @@ export class gameManager extends Component {
 
         let width = this.platformEndTransform.width;
         let positionX = this.platformEnd.position.x;
+
+        let isPerfect = stickPositionX + stickHeight > positionX + this.perfect.position.x && 
+                        stickPositionX < positionX + width - this.perfect.position.x;
+    
+        if (isPerfect) {
+            this.updateScore();
+        }
 
         let isWin = stickPositionX + stickHeight > positionX && stickPositionX + stickHeight < positionX + width;
 
@@ -160,9 +184,40 @@ export class gameManager extends Component {
             })
             .to(1.5, {})
             .call(() => {
+                this.screenShaking(10);
+            })
+            .to(0.1, {})
+            .call(() => {
+                this.screenShaking(-10);
+            })
+            .to(0.1, {})
+            .call(() => {
+                this.screenShaking(10);
+            })
+            .to(0.1, {})
+            .call(() => {
+                this.screenShaking(-10);
+            })
+            .to(0.1, {})
+            .call(() => {
+                this.screenShaking(-10);
+            })
+            .to(0.1, {})
+            .call(() => {
+                this.screenShaking(10);
+            })
+            .to(0.1, {})
+            .call(() => {
                 this.showGameOverScreen();
             })
             .start();
+    }
+
+    private screenShaking(x){
+        this.moveActionPosition(
+            this.node,0.2, 
+            new Vec3(this.node.position.x - x, this.node.position.y, this.node.position.z)
+        ).start();
     }
 
     private resetStick() {
@@ -181,7 +236,7 @@ export class gameManager extends Component {
             this.stickStart.position.z);
     }
 
-    private resize() {
+    private resizeStick() {
         if (this.isResizing) {
             this.stickStartTransform.setContentSize(
                 this.stickStartTransform.contentSize.x, 
@@ -242,26 +297,33 @@ export class gameManager extends Component {
             .call(() => {
                 this.platformStartTransform.width = this.platformEndTransform.width;
                 this.platformStart.position = this.platformEnd.position.clone();
-                this.platformEndTransform.width = Math.round(Math.random() * (200 - 20) + 20);
-                this.platformEnd.position = new Vec3(
-                    screen.width - this.platformEndTransform.width, 
-                    this.platformEnd.position.y, 
-                    this.platformEnd.position.z
-                    );
-                const minX = this.platformStart.position.x + this.platformStartTransform.width + 100;
-                const maxX = this.platformStart.position.x + view.getVisibleSize().width - this.platformEndTransform.width;
-                this.moveActionPosition(
-                    this.platformEnd, 0.5, 
-                    new Vec3(
-                        Math.round(randomRange(minX, maxX)),
-                        this.platformStart.position.y,
-                        this.platformStart.position.z
-                )).start() ;
+                
+                this.movePlatformEnd();
             })
             .call(() => {
                 this.isMoving = false;
             })
             .start();
+    }
+
+    private movePlatformEnd(){
+        this.platformEndTransform.width = Math.round(Math.random() * (200 - 20) + 20);
+        this.perfect.position.set(Math.round(this.platformEndTransform.width/2 - 3), this.perfect.position.y,this.perfect.position.z);
+        this.platformEnd.position = new Vec3(
+            screen.width - this.platformEndTransform.width, 
+            this.platformEnd.position.y, 
+            this.platformEnd.position.z
+        );
+        const minX = this.platformStart.position.x + this.platformStartTransform.width + 100;
+        const maxX = this.platformStart.position.x + view.getVisibleSize().width - this.platformEndTransform.width;
+        let positionX = Math.round(randomRange(minX, maxX));
+        this.moveActionPosition(
+            this.platformEnd, 0.5, 
+            new Vec3(
+                positionX,
+                this.platformStart.position.y,
+                this.platformStart.position.z
+        )).start() ;
     }
 
     private moveActionPosition (object: Node, duration: number, vec: Vec3) {
--- build/web-desktop/index.3dc27.js
+++ build/web-desktop/index.3dc27.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.6359d.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
@@ -8,8 +8,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_application6359dJs) {
+      Application = _application6359dJs.Application;
     }],
     execute: function () {
       application = new Application();
